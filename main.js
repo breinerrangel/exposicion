833d0d2e793e585815598deacdd85e6551d06d76
breiner <rangelbreiner19@gmail.com>
2023-03-26 09:15:04 -0500

    formulario

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 let crear=document.getElementById("crear")
 
 crear.addEventListener("click",()=>{
-    document.getElementById("box").style.display="flex"
+    document.querySelector(".super-box").style.display="flex"
 })
+document.getElementById("Nombre").addEventListener("click",()=>{
+   document.getElementById("Nombre").style.overflow="none"
+ 
+
+})
+
+  
+    
+
+
+
