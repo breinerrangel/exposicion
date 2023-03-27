a2f15bfd4c803754b894a2b2442c08a5f2ddda1f
Rene <rene18agosto@gmail.com>
2023-03-26 21:57:23 -0500

    .

--- main.js
+++ main.js
@@ -1,36 +1,59 @@
-let btnForm = document.querySelector('#btn-form')
-var idList = [];
-class Task {
-    constructor(id, nombre, descripcion, vencTarea) {
-      this.id = id;
-      this.nombre = nombre;
-      this.descripcion = descripcion;
-      this.vencTarea = vencTarea;
-      this.completed = false;
-    }
-  
-    tareaCompletada() {
-      this.completed = true;
-    }
-  
-    nuevaTarea(nombre, descripcion, vencTarea) {
-      this.nombre = nombre;
-      this.descripcion = descripcion;
-      this.vencTarea = vencTarea;
-    }
-  }
-  
-function nuevaId() {
-    var numeroAleatorio = Math.floor(Math.random() * 10000);
-    var newId = numeroAleatorio
+// const person = {
+// 	name: 'John',
+// 	age: 30,
+// 	email: 'john@example.com'
+// };
 
-    while (idList.includes(newId)) {
-        numeroAleatorio = Math.floor(Math.random() * 10000);
-        newId = numeroAleatorio
-    }
-    idList.push(newId);
-    return newId;
-}
+// console.log(person.name); // Output: 'John'
+// console.log(person.age); // Output: 30
+// console.log(person.email); // Output: 'john@example.com'
 
-console.log();
 
+
+// const person = {
+// 	name: 'John',
+// 	age: 38,
+// };
+
+// person.email = 'john@example.com';
+// delete person.age;
+// console.log(person); // Output: { name: 'John', email: john@example.com' }
+
+
+
+// const person = {
+// 	name: 'John',
+// 	age: 38,
+// 	saludo() {
+// 		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+// 	}
+// };
+
+// person.saludo(); // Output: 'Hello, my name is John and I am 30 years old.'
+
+
+
+
+const phoneBook = {
+	'John Doe': '555-1234',
+	'Jane Smith': '555-5678',
+	'Bob Johnson': '555-9612'
+};
+
+console.log(phoneBook['Jane Smith']); // Output: '555-5678"
+
+
+
+function newPerson(name, age) {
+	return {
+		name,
+		age,
+		saludo() {
+			console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+		}
+	};
+};
+const person1 = new Person('John', 38);
+const person2 = new Person('Jane', 25);
+person1.saludo(); // Output: 'Hello, my name is John and I am 30 years old."
+person2.saludo(); // Output: 'Hello, my name is Jane and I am 25 years old."
